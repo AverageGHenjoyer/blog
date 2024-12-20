2a7c31661b8bf6751e97875b321dd5897385c7fa
Maciej Kowalski <maciej.ko1444@gmail.com>
2024-12-20 11:32:07 +0100

    detail.html not done

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.http import Http404
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, '/blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'posts': posts})
 
 
 def post_detail(request, id):
